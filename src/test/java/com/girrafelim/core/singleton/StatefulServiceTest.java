7459522c1f7df67dd5b144f38107fa4a079d6a24
taeyanglim <taeyang7704@naver.com>
2020-10-13 15:23:31 +0900

    스프링 빈은 무상태(stateless)로 설계해야한다.

---
+++ src/test/java/com/girrafelim/core/singleton/StatefulServiceTest.java
@@ -0,0 +1,39 @@
+package com.girrafelim.core.singleton;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class StatefulServiceTest {
+
+    @Test
+    public void statefulServiceSingleton() throws Exception {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(TestConfig.class);
+        StatefulService statefulService1 = ac.getBean(StatefulService.class);
+        StatefulService statefulService2 = ac.getBean(StatefulService.class);
+
+        // Thread A: A 사용자가 10000원을 주문
+        statefulService1.order("userA", 10000);
+        // Thread B: B 사용자가 20000원을 주문
+        statefulService2.order("userB", 20000);
+
+        // Thread A: 사용자 A가 주문 금액 조회
+        int price = statefulService1.getPrice();
+        System.out.println("price = " + price);
+
+        assertThat(statefulService1.getPrice()).isEqualTo(20000);
+    }
+
+    static class TestConfig {
+        @Bean
+        public StatefulService statefulService() {
+            return new StatefulService();
+        }
+    }
+
+}
